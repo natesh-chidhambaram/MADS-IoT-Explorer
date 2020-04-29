293785d780d00b29a1029c65735a776f5b63b422
arjun289 <arjun289singh@gmail.com>
2020-04-29 17:44:18 +0800

    Remove domain www.datakrewtech.com cert check

--- devops/staging/init-letsencrypt.sh
+++ devops/staging/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(datakrewtech.com www.datakrewtech.com)
+domains=(datakrewtech.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="arjun@datakrew.com" # Adding a valid address is strongly recommended
