877bf91ea4f36d3865a9d2c160b771f13fce78f9
Sandesh Soni <sandeshsoni1@gmail.com>
2021-10-27 11:31:01 +0530

    schema. TODO - verify embedded schema, fix changesets.

--- apps/acqdat_core/lib/acqdat_core/reports/schema/template.ex
+++ apps/acqdat_core/lib/acqdat_core/reports/schema/template.ex
@@ -1,18 +1,46 @@
 defmodule AcqdatCore.Reports.Schema.Template do
   use AcqdatCore.Schema
+  alias AcqdatCore.Schema.RoleManagement.User
 
-  schema("acqdat_reports_template") do
-    field(:uuid, :string, null: false)
-    field(:slug, :string, null: false)
+  schema("acqdat_reports_templates") do
+    field(:uuid, :string)
     field(:name, :string)
+    field(:type, :string)
+
+    belongs_to(:creator, User,
+    references: :id,
+    foreign_key: :created_by_user_id
+  )
 
     embeds_many :pages, Pages, on_replace: :delete do
       field(:page_number, :string, null: false)
 
       embeds_many :elements, PageElements, on_replace: :delete do
-        field(:visual_settings, {:array, :map})
-        field(:data_settings, {:array, :map})
+        field(:visual_settings, :map)
+        field(:data_settings, :map)
       end
     end
   end
+
+  @required ~w(pages name uuid)a
+  @optional ~w(type)a
+  @permitted @optional ++ @required
+
+  def changeset(%__MODULE__{} = template, params) do
+    template
+    |> cast(params, @permitted)
+    |> validate_required(@required)
+    # |> common_changeset(params)
+
+  end
+
+  def update_changeset(%__MODULE__{} = template, params) do
+    #
+  end
+
+  # def common_changeset(changeset, _params) do
+  #   changeset
+  #   |> assoc_constraint(:role)
+  # end
+
 end
