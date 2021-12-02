5a4008bb1a435c871ee4de3a4e9fe6e7f0147d56
virusnitesh <nitesh.ranjan.gaya@gmail.com>
2021-12-02 19:22:23 +0530

    create cockpit users table

---
+++ apps/acqdat_core/priv/repo/migrations/20211202133437_create_cockpit_users.exs
@@ -0,0 +1,20 @@
+defmodule AcqdatCore.Repo.Migrations.CreateCockpitUsers do
+  use Ecto.Migration
+
+  def change do
+    execute("CREATE EXTENSION IF NOT EXISTS citext")
+
+    create table("cockpit_users") do
+      add(:first_name, :string, null: false)
+      add(:last_name, :string)
+      add(:email, :citext, null: false)
+      add(:password_hash, :string, null: false)
+      add(:phone_number, :string)
+      add(:avatar, :string)
+
+      timestamps(type: :timestamptz)
+    end
+
+    create unique_index("cockpit_users", [:email])
+  end
+end
