5a2ee8e550f3494b5d3d184411cd1c2ba8618c25
virusnitesh <nitesh.ranjan.gaya@gmail.com>
2021-12-06 17:05:45 +0530

    changes id to uuid

--- apps/acqdat_core/priv/repo/migrations/20211206112639_create_cockpit_users.exs
+++ apps/acqdat_core/priv/repo/migrations/20211206112639_create_cockpit_users.exs
@@ -4,7 +4,9 @@ defmodule AcqdatCore.Repo.Migrations.CreateCockpitUsers do
   def change do
     execute("CREATE EXTENSION IF NOT EXISTS citext")
 
-    create table("cockpit_users") do
+    # @primary_key false
+    create table("cockpit_users", primary_key: false) do
+      add(:id, :uuid, primary_key: true)
       add(:first_name, :string, null: false)
       add(:last_name, :string)
       add(:email, :citext, null: false)
