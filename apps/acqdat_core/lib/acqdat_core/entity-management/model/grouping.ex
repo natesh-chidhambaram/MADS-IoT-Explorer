110e67648a58272e764a9cbf82febfd3da48c9c8
Ayoush Chourasia <ayoushchourasia1418@gmail.com>
2021-12-25 21:14:33 +0530

    Some fixes and added model and schema for alerts in entity management

--- apps/acqdat_core/lib/acqdat_core/entity-management/model/grouping.ex
+++ apps/acqdat_core/lib/acqdat_core/entity-management/model/grouping.ex
@@ -24,25 +24,25 @@ defmodule AcqdatCore.EntityManagement.Model.Grouping do
   from the `alert_policy_meta`.
   """
 
-  # alias AcqdatCore.Alerts.Model.Alert
-  # alias AcqdatCore.AlertMessage.Token
-  # alias Ecto.Multi
-  # alias AcqdatCore.Repo
-  # require Logger
+  alias AcqdatCore.EntityManagement.Model.Alert
+  alias AcqdatCore.AlertMessage.Token
+  alias Ecto.Multi
+  alias AcqdatCore.Repo
+  require Logger
 
-  # def create_alert(%Token{grouping_meta: grp_meta} = params) when grp_meta == %{} do
-  #   alert_data = params |> Map.from_struct() |> Map.put(:status, "un_resolved")
-  #   insert_and_notify(alert_data, params)
-  # end
+  def create_alert(%Token{grouping_meta: grp_meta} = params) when grp_meta == %{} do
+    alert_data = params |> Map.from_struct() |> Map.put(:status, "un_resolved")
+    insert_and_notify(alert_data, params)
+  end
 
-  # def create_alert(%Token{} = params) do
-  #   hash = create_hash(params)
-  #   grouping_hash = %{grouping_hash: hash}
+  def create_alert(%Token{} = params) do
+    hash = create_hash(params)
+    grouping_hash = %{grouping_hash: hash}
 
-  #   grouping_hash
-  #   |> Alert.get()
-  #   |> update_and_notify(params, hash)
-  # end
+    grouping_hash
+    |> Alert.get()
+    |> update_and_notify(params, hash)
+  end
 
   def list_grouping() do
     Enum.reduce(EntityAlertGroupingEnum.__enum_map__(), [], fn {key, value}, acc ->
@@ -55,147 +55,161 @@ defmodule AcqdatCore.EntityManagement.Model.Grouping do
   # Note that we are working separately with alert and alert_event_logs because
   # we essentially want to work with one event log at the given time.
   # See `cast_assoc` and `put_assoc` in Ecto module for more info.
-  # defp update_and_notify({:ok, alert}, params, _grouping_hash) do
-  #   alert_update_params = prepare_update_params(params)
+  defp update_and_notify({:ok, alert}, params, _grouping_hash) do
+    alert_update_params = prepare_update_params(params)
 
-  #   Multi.new()
-  #   |> Multi.run(:update_alert, fn _, _ ->
-  #     Alert.update(alert, alert_update_params)
-  #   end)
-  #   |> Multi.run(:create_alert_log, fn _, %{update_alert: alert} ->
-  #     alert_event_data = prepare_log_params(params, alert.id)
-  #     Alert.create_alert_log(alert_event_data)
-  #   end)
-  #   |> Repo.transaction()
-  #   |> case do
-  #     {:ok, %{update_alert: new_alert, create_alert_log: log}} ->
-  #       check_eligibility(new_alert, alert, params, log)
+    Multi.new()
+    |> Multi.run(:update_alert, fn _, _ ->
+      Alert.update(alert, alert_update_params)
+    end)
+    |> Multi.run(:create_alert_log, fn _, %{update_alert: alert} ->
+      alert_event_data = prepare_log_params(params, alert.id)
+      Alert.create_alert_log(alert_event_data)
+    end)
+    |> Repo.transaction()
+    |> case do
+      {:ok, %{update_alert: new_alert, create_alert_log: log}} ->
+        check_eligibility(new_alert, alert, params, log)
 
-  #     {:error, failed_operation, failed_value, _changes_so_far} ->
-  #       {:error, {failed_operation, failed_value}}
-  #   end
-  # end
+      {:error, failed_operation, failed_value, _changes_so_far} ->
+        {:error, {failed_operation, failed_value}}
+    end
+  end
 
-  # defp update_and_notify({:error, _message}, params, grouping_hash) do
-  #   alert_data = prepare_insert_params(params, grouping_hash)
-  #   insert_and_notify(alert_data, params)
-  # end
+  defp update_and_notify({:error, _message}, params, grouping_hash) do
+    alert_data = prepare_insert_params(params, grouping_hash)
+    insert_and_notify(alert_data, params)
+  end
 
-  # defp insert_and_notify(alert_data, params) do
-  #   Multi.new()
-  #   |> Multi.run(:create_alert, fn _, _ ->
-  #     Alert.create(alert_data)
-  #   end)
-  #   |> Multi.run(:create_alert_log, fn _, %{create_alert: alert} ->
-  #     alert_event_data = prepare_log_params(params, alert.id)
-  #     Alert.create_alert_log(alert_event_data)
-  #   end)
-  #   |> Repo.transaction()
-  #   |> case do
-  #     {:ok, %{create_alert: alert, create_alert_log: log}} ->
-  #       {:ok, {alert, log}}
-  #       send_alert(alert, log)
+  defp insert_and_notify(alert_data, params) do
+    Multi.new()
+    |> Multi.run(:create_alert, fn _, _ ->
+      Alert.create(alert_data)
+    end)
+    |> Multi.run(:create_alert_log, fn _, %{create_alert: alert} ->
+      alert_event_data = prepare_log_params(params, alert.id)
+      Alert.create_alert_log(alert_event_data)
+    end)
+    |> Repo.transaction()
+    |> case do
+      {:ok, %{create_alert: alert, create_alert_log: log}} ->
+        {:ok, {alert, log}}
+        send_alert(alert, log)
 
-  #     {:error, failed_operation, failed_value, _changes_so_far} ->
-  #       Logger.error("#{inspect(failed_operation)}")
-  #       Logger.error("#{inspect(failed_value)}")
+      {:error, failed_operation, failed_value, _changes_so_far} ->
+        Logger.error("#{inspect(failed_operation)}")
+        Logger.error("#{inspect(failed_value)}")
 
-  #       Logger.error("Alert sending failed",
-  #         failed_operation: failed_operation,
-  #         failed_value: failed_value
-  #       )
+        Logger.error("Alert sending failed",
+          failed_operation: failed_operation,
+          failed_value: failed_value
+        )
 
-  #       {:error, {failed_operation, failed_value}}
-  #   end
-  # end
+        {:error, {failed_operation, failed_value}}
+    end
+  end
 
-  # defp create_hash(params) do
-  #   hash_params =
-  #     %{}
-  #     |> Map.put(:alert_policy_meta, params.alert_policy_meta)
-  #     |> Map.put(:app, params.app)
-  #     |> Map.put(:entity_name, params.entity_name)
-  #     |> Map.put(:entity_id, params.entity_id)
-  #     |> Map.put(:org_id, params.org_id)
+  defp create_hash(params) do
+    hash_params =
+      %{}
+      |> Map.put(:alert_policy_meta, params.alert_policy_meta)
+      |> Map.put(:app, params.app)
+      |> Map.put(:entity_name, params.entity_name)
+      |> Map.put(:entity_id, params.entity_id)
+      |> Map.put(:org_id, params.org_id)
 
-  #   to_string(Murmur.hash_x64_128(hash_params))
-  # end
+    to_string(Murmur.hash_x64_128(hash_params))
+  end
 
-  # defp check_eligibility(new_alert, alert, params, log) do
-  #   grouping = alert.grouping_meta
+  defp check_eligibility(new_alert, alert, params, log) do
+    grouping = alert.grouping_meta
 
-  #   if grouping.module.run_check?(grouping.grouping_parameters, params) do
-  #     send_alert(new_alert, log)
-  #   else
-  #     :ok
-  #   end
-  # end
+    if grouping.module.run_check?(grouping.grouping_parameters, params) do
+      send_alert(new_alert, log)
+    else
+      :ok
+    end
+  end
 
   # TODO: At present recipient ids are only for user hence the handling is only
   # for that, it needs to be changed for org and projects too later.
-  # defp send_alert(alert, alert_log) do
-  #   alert_log = Map.from_struct(alert_log)
+  defp send_alert(alert, alert_log) do
+    alert_log = Map.from_struct(alert_log)
 
-  #   recipient_ids =
-  #     Enum.map(alert.recipient_ids, fn recipient ->
-  #       recipient.id
-  #     end)
+    recipient_ids =
+      Enum.map(alert.recipient_ids, fn recipient ->
+        recipient.id
+      end)
 
-  #   app =
-  #     Atom.to_string(alert.app)
-  #     |> String.split("_")
-  #     |> Enum.reduce([], fn x, acc -> acc ++ [String.capitalize(x)] end)
-  #     |> Enum.reduce("", fn x, acc -> acc <> x <> " " end)
-  #     |> String.trim_trailing(" ")
+    app =
+      Atom.to_string(alert.app)
+      |> String.split("_")
+      |> Enum.reduce([], fn x, acc -> acc ++ [String.capitalize(x)] end)
+      |> Enum.reduce("", fn x, acc -> acc <> x <> " " end)
+      |> String.trim_trailing(" ")
 
-  #   alert_log =
-  #     alert_log
-  #     |> Map.put(:app, app)
-  #     |> Map.put(:communication_medium, alert.communication_medium)
-  #     |> Map.put(:recipient_ids, recipient_ids)
+    alert_log =
+      alert_log
+      |> Map.put(:app, app)
+      |> Map.put(:communication_medium, alert.communication_medium)
+      |> Map.put(:recipient_ids, recipient_ids)
 
-  #   Notifications.send_notifications(alert_log, "twilio")
-  # end
+    Notifications.send_notifications(alert_log, "twilio")
+  end
 
   # TODO: optimize creating grouping_meta
-  # defp prepare_insert_params(%Token{} = params, grouping_hash) do
-  #   grouping_params =
-  #     Map.put(params.grouping_meta.grouping_parameters, :previous_time, params.inserted_timestamp)
+  defp prepare_insert_params(%Token{} = params, grouping_hash) do
+    grouping_params =
+      case Map.has_key?(params.grouping_meta.grouping_parameters, :unit) do
+        true ->
+          Map.put(
+            params.grouping_meta.grouping_parameters,
+            :previous_time,
+            params.inserted_timestamp
+          )
 
-  #   grouping_meta = Map.put(params.grouping_meta, :grouping_parameters, grouping_params)
+        false ->
+          Map.put(
+            params.grouping_meta.grouping_parameters,
+            "previous_time",
+            params.inserted_timestamp
+          )
+      end
 
-  #   alert_params = Map.from_struct(params)
+    grouping_meta = Map.put(params.grouping_meta, :grouping_parameters, grouping_params)
 
-  #   alert_params
-  #   |> Map.put(:grouping_hash, grouping_hash)
-  #   |> Map.put(:status, "un_resolved")
-  #   |> Map.put(:grouping_meta, grouping_meta)
-  # end
+    alert_params = Map.from_struct(params)
 
-  # defp prepare_update_params(%Token{} = params) do
-  #   grouping_params =
-  #     Map.put(params.grouping_meta.grouping_parameters, :previous_time, params.inserted_timestamp)
+    alert_params
+    |> Map.put(:grouping_hash, grouping_hash)
+    |> Map.put(:status, "un_resolved")
+    |> Map.put(:grouping_meta, grouping_meta)
+  end
 
-  #   grouping_meta = Map.put(params.grouping_meta, :grouping_parameters, grouping_params)
+  defp prepare_update_params(%Token{} = params) do
+    grouping_params =
+      Map.put(params.grouping_meta.grouping_parameters, :previous_time, params.inserted_timestamp)
 
-  #   %{
-  #     name: params.name,
-  #     description: params.description,
-  #     communication_medium: params.communication_medium,
-  #     status: "un_resolved",
-  #     severity: params.severity,
-  #     grouping_meta: grouping_meta
-  #   }
-  # end
+    grouping_meta = Map.put(params.grouping_meta, :grouping_parameters, grouping_params)
 
-  # defp prepare_log_params(%Token{} = params, alert_id) do
-  #   %{
-  #     inserted_timestamp: params.inserted_timestamp,
-  #     name: params.name,
-  #     description: params.description,
-  #     severity: params.severity,
-  #     alert_metadata: params.alert_log,
-  #     alert_id: alert_id
-  #   }
-  # end
+    %{
+      name: params.name,
+      description: params.description,
+      communication_medium: params.communication_medium,
+      status: "un_resolved",
+      severity: params.severity,
+      grouping_meta: grouping_meta
+    }
+  end
+
+  defp prepare_log_params(%Token{} = params, alert_id) do
+    %{
+      inserted_timestamp: params.inserted_timestamp,
+      name: params.name,
+      description: params.description,
+      severity: params.severity,
+      alert_metadata: params.alert_log,
+      alert_id: alert_id
+    }
+  end
 end
---
+++ apps/acqdat_core/lib/acqdat_core/entity-management/schema/alerts/alert.ex
@@ -0,0 +1,120 @@
+defmodule AcqdatCore.Schema.EntityManagement.Alert do
+  use AcqdatCore.Schema
+  alias AcqdatCore.EntityManagement.Schema.Grouping
+  alias AcqdatCore.EntityManagement.Schema.Alert.Recipient
+  alias AcqdatCore.Schema.EntityManagement.Organisation
+  alias AcqdatCore.Alerts.Schema.AlertEventLog
+
+  @typedoc """
+  `name`: A heading for the alert
+  `description`: Description of the alert. This is what the user will see.
+  `alert_policy_meta`: A map containing all the data due to which the alert was
+    raised, it should be a map containing all the information. The alert policy meta
+    should be a consistent meta and shouldn't change a lot as it used for grouping
+    the alerts together.
+  `grouping_meta`: Data related to grouping of the alerts raised from this particular
+    policy.
+  `grouping_hash`: The grouping hash is created from the combination of
+    `alert_policy_meta`, `entity_name`, `entity_uuid`, `org_uuid` and `project_uuid`.
+    It's used for uniquely idenitifying an alert and group similar events together.
+  `entity_name`: Entity for which the alert was raised, this can be optional.
+  `entity_id`: uuid for the entity.
+  `communicaton_medium`: Communication medium which should be used for sending the
+    alert. It can be `email`, `sms` or `in-app`.
+  `recipient_ids`: Ids of the users who should be notified using the communication
+    mediums.
+  `severity`: Severity of the alert. Severity can be `high`, `low` or `medium`
+  `status`: A flag to show if the alert is resolved.
+  `app`: Shows which app raised the alert.
+  `org_id`: id of the organisation for which the alert was raised.
+  `project_id`: id of the project for which alert happened. This is optional.
+  `alert_events_log`: Holds all the data alongwith timestamp about the alert
+    event.
+  `alert_meta`: Holds metadata related to the alert.
+  ``
+  """
+
+  @type t :: %__MODULE__{}
+
+  schema "acqdat_alerts" do
+    ## Fields from the token sent by an app.
+    field(:name, :string, null: false)
+    field(:description, :string)
+    field(:alert_policy_meta, :map)
+    field(:entity_name, :string)
+    field(:entity_id, :integer)
+    field(:communication_medium, {:array, :string})
+    field(:severity, EntityAlertSeverityEnum)
+    field(:app, EntityAppEnum)
+    field(:project_id, :integer)
+
+    ## Fields that would be generated later on.
+    field(:grouping_hash, :string)
+    field(:status, EntityAlertStatusEnum)
+    field(:alert_meta, :map)
+
+    ## Embeds
+    embeds_one(:grouping_meta, Grouping, on_replace: :update)
+    embeds_many(:recipient_ids, Recipient)
+
+    ## associations
+    belongs_to(:org, Organisation, on_replace: :delete)
+    has_many(:alert_event_log, AlertEventLog)
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @required_params ~w(name app communication_medium
+    severity status org_id)a
+  @optional_params ~w(alert_policy_meta description entity_name entity_id project_id
+    grouping_hash alert_meta)a
+  @permitted_params @required_params ++ @optional_params
+
+  @comm_medium ~w(e-mail in-app sms)s
+
+  def changeset(%__MODULE__{} = alert, params) do
+    alert
+    |> cast(params, @permitted_params)
+    |> cast_embed(:recipient_ids)
+    |> cast_embed(:grouping_meta, with: &Grouping.changeset/2)
+    |> validate_required(@required_params)
+    |> validate_subset(:communication_medium, @comm_medium)
+    |> unique_constraint(:grouping_hash,
+      name: :unique_grouping_hash_per_alert,
+      message: "unqiue hash per alert should be generated"
+    )
+    |> assoc_constraint(:org)
+  end
+
+  @update_parameters ~w(name description
+    communication_medium status severity alert_meta)a
+
+  def update_changeset(%__MODULE__{} = alert, params) do
+    alert
+    |> cast(params, @update_parameters)
+    |> cast_embed(:grouping_meta, with: &Grouping.changeset/2)
+    |> cast_embed(:recipient_ids)
+    |> validate_subset(:communication_medium, @comm_medium)
+  end
+end
+
+defmodule AcqdatCore.EntityManagement.Schema.Alert.Recipient do
+  use AcqdatCore.Schema
+
+  # TODO: There should be a field to store communication related info such as email
+  #      of the user, or phone number to which the alert shold be sent
+
+  @primary_key false
+  embedded_schema do
+    field(:type, :string)
+    field(:id, :integer)
+  end
+
+  @params ~w(type id)a
+
+  def changeset(%__MODULE__{} = recipient, params) do
+    recipient
+    |> cast(params, @params)
+    |> validate_required(@params)
+  end
+end
